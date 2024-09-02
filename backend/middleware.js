a34a30c96b42d8ef5275c2e28ce0f381e2ecc53e
rajatsaini2003 <rajatsaini9460@gmail.com>
2024-09-02 23:16:53 +0530

    auth middleware add

--- backend/middleware.js
+++ backend/middleware.js
@@ -2,5 +2,31 @@ require('dotenv').config();
 const jwt =require('jsonwebtoken');
 
 const authMiddleware = (req,res,next) => {
-    
+    const authHeader=req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        return res.status(403).json({
+            "message":err
+        });
+    }
+    const token = authHeader.split(' ')[1];
+
+    try{
+        const decoder = jwt.verify(token, process.env.JWT.SECRET);
+        if(decoder.userId){
+            req.userId = decoder.userId;
+            next();
+        } else{
+            return res.statue(403).json({
+                "message":err
+            });
+        }
+    }
+    catch(err){
+        return res.statue(403).json({
+            "message":err
+        });
+    }
+
 }
+module.exports = authMiddleware;
